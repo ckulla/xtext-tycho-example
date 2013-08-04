d2a2ad5dd098b4f145763d956580b5912865505d
Christoph Kulla <christophkulla@googlemail.com>
2013-08-04 19:27:29 +0200

    Stripped down the example project to not include the JDT. Added a standalone repository which will provide the standalone code generator in future.

--- plugins/org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/MyDslUiModule.java
+++ plugins/org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/MyDslUiModule.java
@@ -4,6 +4,11 @@
 package org.xtext.example.mydsl.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.model.IResourceForEditorInputFactory;
+import org.eclipse.xtext.ui.editor.model.ResourceForIEditorInputFactory;
+import org.eclipse.xtext.ui.resource.IResourceSetProvider;
+import org.eclipse.xtext.ui.resource.SimpleResourceSetProvider;
+import org.eclipse.xtext.ui.shared.Access;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -11,5 +16,22 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 public class MyDslUiModule extends org.xtext.example.mydsl.ui.AbstractMyDslUiModule {
 	public MyDslUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
+	}
+	
+	// prevent JDT dependencies, see https://bugs.eclipse.org/bugs/show_bug.cgi?id=404322
+	
+	@Override
+	public com.google.inject.Provider<org.eclipse.xtext.resource.containers.IAllContainersState> provideIAllContainersState() {
+		return Access.getWorkspaceProjectsState();
+	}
+	
+	@Override
+	public Class<? extends IResourceSetProvider> bindIResourceSetProvider() {
+		return SimpleResourceSetProvider.class;
+	}
+
+	@Override
+	public Class<? extends IResourceForEditorInputFactory> bindIResourceForEditorInputFactory() {
+		return ResourceForIEditorInputFactory.class;
 	}
 }
