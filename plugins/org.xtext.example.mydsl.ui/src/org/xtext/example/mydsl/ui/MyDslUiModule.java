33da26c93611b19efbd8c059b293024dca96d287
Christoph Kulla <christophkulla@googlemail.com>
2013-12-27 14:13:59 +0100

    Fixed issue with IStorage2UriMapperJdtExtensions, see https://bugs.eclipse.org/bugs/show_bug.cgi?id=424455

--- plugins/org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/MyDslUiModule.java
+++ plugins/org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/MyDslUiModule.java
@@ -7,13 +7,18 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.xtext.ui.editor.model.IResourceForEditorInputFactory;
 import org.eclipse.xtext.ui.editor.model.ResourceForIEditorInputFactory;
 import org.eclipse.xtext.ui.resource.IResourceSetProvider;
+import org.eclipse.xtext.ui.resource.IStorage2UriMapperJdtExtensions;
 import org.eclipse.xtext.ui.resource.SimpleResourceSetProvider;
 import org.eclipse.xtext.ui.shared.Access;
+
+import com.google.inject.Binder;
+import com.google.inject.util.Providers;
 
 /**
  * Use this class to register components to be used within the IDE.
  */
 public class MyDslUiModule extends org.xtext.example.mydsl.ui.AbstractMyDslUiModule {
+	
 	public MyDslUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
 	}
@@ -33,5 +38,11 @@ public class MyDslUiModule extends org.xtext.example.mydsl.ui.AbstractMyDslUiMod
 	@Override
 	public Class<? extends IResourceForEditorInputFactory> bindIResourceForEditorInputFactory() {
 		return ResourceForIEditorInputFactory.class;
+	}
+
+	// FIXME: due to "Xtext based editor does not start since 2.5 without JDT installed", 
+	// https://bugs.eclipse.org/bugs/show_bug.cgi?id=424455
+	public void configureIStorage2UriMapperJdtExtensions(Binder binder) {
+		binder.bind(IStorage2UriMapperJdtExtensions.class).toProvider(Providers.of((IStorage2UriMapperJdtExtensions)null));
 	}
 }
